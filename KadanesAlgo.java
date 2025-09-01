614ee9242214b831ba58053df55ec0874343f577
Tanishq Sharma <168180518+TanishqSharma22@users.noreply.github.com>
2025-09-02 01:08:57 +0530

    Add files via upload

---
+++ KadanesAlgo.java
@@ -0,0 +1,26 @@
+package DSA;
+
+public class KadanesAlgo {
+    public static long maxSubarraySum(int[] arr, int n) {
+        long maxi = Long.MIN_VALUE;
+        long sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+
+            if(sum > maxi){
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+    public static void main(String args[]) {
+        int[] arr = {2,3,5,-2,7,-4};
+        int n = arr.length;
+        long maxSum = maxSubarraySum(arr, n);
+        System.out.print(maxSum);
+
+    }
+}
