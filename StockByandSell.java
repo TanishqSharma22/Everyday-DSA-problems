974e16f4fbe96cef2f23b7fd8a5b15f070f4873c
Tanishq Sharma <168180518+TanishqSharma22@users.noreply.github.com>
2025-09-02 13:23:48 +0530

    Add files via upload

---
+++ StockByandSell.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class StockByandSell {
+    public static int maxProfit(int[] arr) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]-min);
+
+        }
+        return max;
+
+    }
+    public static void main(String args[]) {
+        int arr[] = {7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
+    }
+    
+}
